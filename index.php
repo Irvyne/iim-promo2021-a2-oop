230d7f754bbb5ad84d638cc304d82eb892955520
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-12-06 19:04:46 +0100

    Faker

--- index.php
+++ index.php
@@ -4,3 +4,17 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require __DIR__ . '/bootstrap.php';
+
+use App\Entity\Article;
+
+$faker = Faker\Factory::create();
+
+for ($i = 0; $i < 100000; $i++) {
+    $article = new Article();
+    $article->setName($faker->name);
+    $article->setStatus(Article::STATUS_UNPUBLISHED);
+
+    $entityManager->persist($article);
+}
+
+$entityManager->flush();
