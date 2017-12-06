d95e3b3e83050f559f56bcf047719b655a4fb177
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-12-06 19:05:40 +0100

    Faker

--- index.php
+++ index.php
@@ -15,6 +15,11 @@ for ($i = 0; $i < 100000; $i++) {
     $article->setStatus(Article::STATUS_UNPUBLISHED);
 
     $entityManager->persist($article);
+
+    // Save all data each 500 articles to avoid memory leaks.
+    if (0 === $i % 500) {
+        $entityManager->flush();
+    }
 }
 
 $entityManager->flush();
