2c607582df3199f52a53893b77df49000745c74a
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-12-11 15:18:00 +0100

    Pagination

--- articles.php
+++ articles.php
@@ -11,14 +11,37 @@ use App\Entity\Article;
 $page = $_GET['page'] ?? 1;
 
 /** @var \App\Repository\ArticleRepository $repo */
-$repo     = $entityManager->getRepository(Article::class);
-$articles = $repo->loadAll(Article::MAX_PER_PAGE, ($page - 1) * Article::MAX_PER_PAGE);
-$count    = $repo->count();
-$maxPage  = (int)ceil($count / Article::MAX_PER_PAGE);
+$repo          = $entityManager->getRepository(Article::class);
+$articles      = $repo->loadAll(Article::MAX_PER_PAGE, ($page - 1) * Article::MAX_PER_PAGE);
+$count         = $repo->count();
+
+$maxPagination = (int)ceil($count / Article::MAX_PER_PAGE);
+
+$minPage = (int)max(1, ($page - 5));
+$maxPage = (int)min($maxPagination, ($page + 5));
+
+$max = 0;
+
+while(abs($minPage - $maxPage) < 10){
+    if ($minPage > 1){
+        $minPage--;
+    }
+    if ($maxPage < $maxPagination){
+        $maxPage++;
+    }
+
+    $max++;
+
+    if ($max > 10){
+        break;
+    }
+}
 
 echo $twig->render('articles.html.twig', [
-    'title'       => 'Articles',
-    'articles'    => $articles,
-    'currentPage' => $page,
-    'maxPage'     => $maxPage,
+    'title'         => 'Articles',
+    'articles'      => $articles,
+    'currentPage'   => $page,
+    'maxPagination' => $maxPagination,
+    'minPage'       => $minPage,
+    'maxPage'       => $maxPage,
 ]);
